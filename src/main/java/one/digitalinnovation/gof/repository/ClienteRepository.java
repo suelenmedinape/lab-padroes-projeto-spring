540dd9648e07dd9091b652c60ecebb41b1984f3e
Suelen Medina <suelenmedinape@gmail.com>
2024-07-02 10:39:55 -0300

    local do repositorio

--- src/main/java/one/digitalinnovation/gof/repository/ClienteRepository.java
+++ src/main/java/one/digitalinnovation/gof/repository/ClienteRepository.java
@@ -1,4 +1,4 @@
-package one.digitalinnovation.gof.model;
+package one.digitalinnovation.gof.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
